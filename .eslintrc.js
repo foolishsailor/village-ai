b6302f0ab63072a3a482dc16a1f57197852e7e69
jc.durbin <jc.durbin@ardanis.com>
2023-04-25 10:11:35 +0100

    boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['tsconfig.json'],
+  rules: {
+    'object-shorthand': 'warn',
+    'no-useless-rename': 'warn',
+    'prefer-destructuring': 'warn',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single']
+  }
+};
